968249296d26c9fbfbab0f81d19b9402edaf1279
Filip Strajnar <filip.strajnar@gmail.com>
2024-01-14 21:47:38 +0100

    Form rendering done.

--- src/seminarska_naloga/__main__.py
+++ src/seminarska_naloga/__main__.py
@@ -1,1 +1,71 @@
-from numpy import array, ndarray
+from flask import Flask, render_template, make_response, request, session, redirect
+from os import urandom, environ
+
+app = Flask(
+    __name__,
+    static_folder="static",
+    static_url_path='/static',
+)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", name="Filip")
+
+
+@app.route("/", methods=["POST"])
+def number_of_criteria():
+    session["alternatives"] = int(request.form["alternatives"])
+    session["criteria"] = int(request.form["criteria"])
+    return redirect("/criteria")
+
+
+@app.route("/criteria")
+def criteria_view():
+    return render_template("criteria.html", number_of_weights=range(session["criteria"]))
+
+
+@app.route("/criteria", methods=["POST"])
+def criteria_submit():
+    criteria = []
+
+    for i in range(session["criteria"]):
+        current_name = request.form[f"name_{i}"]
+        current_value = request.form[f"value_{i}"]
+        criteria.append((current_name, float(current_value)))
+
+    session["criteria_values"] = criteria
+
+    return redirect("/alternatives")
+
+
+@app.route("/alternatives")
+def alternatives_view():
+    return render_template("alternatives.html", number_of_alternatives=range(session["alternatives"]))
+
+
+@app.route("/alternatives", methods=["POST"])
+def alternatives_submit():
+    alternatives = []
+
+    for i in range(session["alternatives"]):
+        current_name = request.form[f"name_{i}"]
+        alternatives.append(current_name)
+
+    session["alternatives_values"] = alternatives
+    return redirect("/values")
+
+
+@app.route("/values")
+def values_view():
+    alternatives = session["alternatives_values"]
+    criteria = session["criteria_values"]
+
+    full_alternatives = [(a, [(c[0], f"{i}_{j}") for j, c in enumerate(criteria)]) for i, a in enumerate(alternatives)]
+    print(full_alternatives)
+    return render_template("values.html", full_alternatives=full_alternatives)
+
+
+app.config.update(SECRET_KEY=urandom(32).hex())
+
+app.run("0.0.0.0", 5000)
